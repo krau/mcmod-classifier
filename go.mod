e4cc6ba1bee7c1bcd634087556743fd6db47f50c
krau <krau39@outlook.com>
2023-11-24 19:51:19 +0800

    第一次提交

---
+++ go.mod
@@ -0,0 +1,42 @@
+module mcmod-classifier
+
+go 1.21.4
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/cloudflare/circl v1.3.6 // indirect
+	github.com/gaukas/godicttls v0.0.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20231101202521-4ca4178f5c7a // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/goutil v0.6.12 // indirect
+	github.com/gookit/gsr v0.1.0 // indirect
+	github.com/gookit/slog v0.5.4 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/imroc/req/v3 v3.42.2 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/onsi/ginkgo/v2 v2.13.1 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-20 v0.4.1 // indirect
+	github.com/quic-go/quic-go v0.40.0 // indirect
+	github.com/refraction-networking/utls v1.5.4 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/schollz/progressbar/v3 v3.14.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	go.uber.org/mock v0.3.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/term v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gookit/slog"
+	"github.com/gookit/slog/handler"
+	"github.com/imroc/req/v3"
+	"github.com/schollz/progressbar/v3"
+)
+
+const (
+	ServerMust     = "服务端需装"
+	ClientMust     = "客户端需装"
+	ServerInvalid  = "服务端无效"
+	ClientInvalid  = "客户端无效"
+	ServerOptional = "服务端可选"
+	ClientOptional = "客户端可选"
+	Unknown        = "未识别"
+)
+
+var (
+	BracketsPattern = regexp.MustCompile(`\[([^]]+)\]`)
+	VersionPattern  = regexp.MustCompile(`\b\d+(\.\d+)+\b|\bv\d+(\.\d+)+\b`)
+	Client          = req.C()
+	WikiURL         = "https://search.mcmod.cn/s"
+	Log             *slog.Logger
+	TargetPath      = []string{ServerMust, ClientMust, ServerInvalid, ClientInvalid, ServerOptional, ClientOptional, Unknown}
+)
+
+func init() {
+	for _, path := range TargetPath {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			os.Mkdir(path, os.ModePerm)
+		}
+	}
+
+	slog.DefaultChannelName = "mcmod-classifier"
+	newLogger := slog.New()
+	defer newLogger.Flush()
+	fileH, err := handler.NewFileHandler("mcmod-classifier.log")
+	if err != nil {
+		panic(err)
+	}
+	newLogger.AddHandler(fileH)
+	Log = newLogger
+}
+
+func GetModName(path string) string {
+	s := filepath.Base(path)
+	s = strings.TrimSuffix(s, ".jar")
+	s = strings.ToLower(s)
+
+	if BracketsPattern.FindStringSubmatch(s) != nil {
+		s = BracketsPattern.FindStringSubmatch(s)[1]
+	}
+
+	if strings.Contains(s, "-forge") {
+		s = strings.Split(s, "-forge")[0]
+	}
+	if strings.Contains(s, "-fabric") {
+		s = strings.Split(s, "-fabric")[0]
+	}
+
+	if strings.Contains(s, "-") {
+		parts := VersionPattern.Split(s, -1)
+		s = strings.TrimSuffix(parts[0], "-")
+	}
+	Log.Debugf("Mod name: %s", s)
+	return s
+}
+
+func CopyFile(src, dst string) {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		Log.Error(err)
+		return
+	}
+	defer srcFile.Close()
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		Log.Error(err)
+		return
+	}
+	defer dstFile.Close()
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		Log.Error(err)
+		return
+	}
+	Log.Debugf("Copied %s to %s", src, dst)
+}
+
+func main() {
+	matches, err := filepath.Glob("mods/*.jar")
+	if err != nil {
+		slog.Error(err)
+		return
+	}
+	slog.Infof("Found %d mods\n", len(matches))
+	bar := progressbar.Default(int64((len(matches))))
+
+	Client.ImpersonateChrome()
+	Client.SetMaxConnsPerHost(1)
+
+	for _, match := range matches {
+		bar.Add(1)
+		time.Sleep(1 * time.Second)
+		modName := GetModName(match)
+		r, err := Client.R().SetQueryParam("key", modName).Get(WikiURL)
+		if err != nil {
+			slog.Error(err)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+			continue
+		}
+		doc, err := goquery.NewDocumentFromReader(r.Body)
+		if err != nil {
+			slog.Error(err)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+			continue
+		}
+		// 获取模组页面链接
+		val, exists := doc.Find(".head").First().Find("a").Eq(1).Attr("href")
+		if !exists {
+			Log.Warnf("Mod %s not found", modName)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+			continue
+		}
+		// 获取模组页面
+		Log.Debugf("Mod %s found, url: %s", modName, val)
+		r, err = Client.R().Get(val)
+		if err != nil {
+			slog.Error(err)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+			continue
+		}
+		doc, err = goquery.NewDocumentFromReader(r.Body)
+		if err != nil {
+			slog.Error(err)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+			continue
+		}
+		// 获取模组运行环境
+		modRunEnv := doc.Find(".class-info-left").Find(".col-lg-12").Find(".col-lg-4").Eq(2).Text()
+		matched := false
+		for _, path := range TargetPath {
+			if strings.Contains(modRunEnv, path) {
+				CopyFile(match, filepath.Join(path, filepath.Base(match)))
+				matched = true
+			}
+		}
+		if !matched {
+			Log.Warnf("Mod %s not matched", modName)
+			CopyFile(match, filepath.Join(Unknown, filepath.Base(match)))
+		}
+	}
+	slog.Info("Done")
+
+}
